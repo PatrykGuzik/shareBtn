5a4670bb8ac86a91e863bf406ce255f9dccbfb96
Patryk Guzik <p.guzik772@gmail.com>
2022-05-16 12:41:16 +0200

    another

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-console.log("test");
 
-const meta = document.head.querySelector('meta[property="og:title"]')
 
-meta.content = "nowa nazwa strony"
 
-console.log(meta.content);
+const metaShareTitle = document.head.querySelector('meta[property="og:title"]');
+
+metaShareTitle.content = "nowa nazwa strony";
+
+
+const shareBtn = document.querySelector(".share-btn")
+
+shareBtn.innerHTML = getShareBtnHTML()
+
+
+
+
+function getShareBtnHTML() {
+    return `
+    <div class="fb-share-button" data-href="https://patrykguzik.github.io/shareBtn/" data-layout="button"
+        data-size="large"><a target="_blank"
+            href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fpatrykguzik.github.io%2FshareBtn%2F&amp;src=sdkpreparse"
+            class="fb-xfbml-parse-ignore">Udostępnij</a></div>
+    `
+}
